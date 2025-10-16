39e72abb2afba9cb9a186c74f8f4c34150e76d8a
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-10-16 17:12:37 -0300

    #17 Implemented test with K6s

---
+++ test/k6/spike_tests.js
@@ -0,0 +1,40 @@
+import http from 'k6/http';
+import { Trend } from 'k6/metrics';
+import { check } from 'k6';
+
+const statusTrend = new Trend('status_codes');
+
+export const options = {
+    stages: [
+        { duration: '10s', target: 1000 },
+        { duration: '20s', target: 1000 },
+        { duration: '10s', target: 0 },
+    ],
+};
+
+const BASE_URL = 'http://localhost:5000/api';
+const payload = JSON.stringify({
+    serverId: 'MSI',
+    cpu_usage: 20.5,
+    ram_usage: 55.3,
+    disk_space: 40.0,
+    temperature: 45.0,
+});
+const params = { headers: { 'Content-Type': 'application/json' } };
+
+export default function () {
+    const res = http.post(`${BASE_URL}/metrics`, payload, params);
+
+    statusTrend.add(res.status);
+
+    check(res, {
+        'status 201': (r) => r.status === 201,
+        // 'status 400': (r) => r.status === 400,
+        // 'status 404': (r) => r.status === 404,
+        // 'status 409': (r) => r.status === 409,
+        // 'status 429': (r) => r.status === 429,
+        // 'status 500': (r) => r.status === 500,
+    });
+}
+
+
